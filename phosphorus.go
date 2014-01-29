aac365db19cdcde53a3fdf66483a570dc5a5ad45
Will St. Clair <will@willstclair.com>
2014-01-29 15:55:48 -0500

    Reorg and purge phosphorus.go

--- phosphorus.go
+++ phosphorus.go
@@ -1,33 +1,19 @@
 package main
 
 import (
-	// "bufio"
-	// "encoding/gob"
-	// "encoding/base64"
+	log_ "log"
+	"fmt"
 	"encoding/json"
-	// "encoding/binary"
-	// "math/rand"
-	// crand "crypto/rand"
-	// "math"
-	// "math/big"
-	// "bytes"
-	"flag"
-	// "fmt"
-	"log"
 	"os"
-	// "path/filepath"
 	"runtime"
-	// "sync"
-
-	// "willstclair.com/metaphone3"
-	"willstclair.com/phosphorus/index"
-	"willstclair.com/phosphorus/encoder"
-	"github.com/crowdmob/goamz/dynamodb"
-
-
 )
 
-const BANNER = `
+const (
+	WORKING_DIR = "/var/lib/phosphorus"
+	WORKING_DIR_ENV = "PHOSPHORUS_DIR"
+	CONFIG_FILE = "config.json"
+	LOG_FLAGS = log_.Ldate | log_.Ltime | log_.Lshortfile
+	BANNER = `
                            )
                           ) \
         . . . . . . . . ./ ) (. . .
@@ -38,83 +24,56 @@ const BANNER = `
     match server      / /
                      /_/
 `
-
-type _Aws struct {
-	AccessKeyID      string
-	SecretAccessKey  string
-	DynamoDBLocalDir string
-}
-
-type Configuration struct {
-	MaxProcs         int
-	WorkingDirectory string
-	SignatureCount   int
-	SignatureBits    int
-	AWS              _Aws
-}
+)
 
 var (
-	configPath string
-	action string
-	inGlob string
-	force bool
-	Config Configuration
-	c lib.Counter
-	e lib.Encoder
+	working string
+	log log_.Logger
+	config Configuration
 )
 
-const (
-	encFile = "encoder"
-)
+type Configuration struct {
+	MaxProcs            int
+	AWSSecretAccessKey  string
+	AWSAccessKeyId      string
+}
 
 func init() {
-	defaultConfigPath := os.Getenv("PHOSPHORUS_CONFIG")
-	if len(defaultConfigPath) == 0 {
-		defaultConfigPath = os.Getenv("HOME") + "/.phosphorus"
+	var err error
+
+	log = *log_.New(os.Stdout, "", LOG_FLAGS)
+
+	working = os.Getenv(WORKING_DIR_ENV)
+	if working == "" {
+		working = WORKING_DIR
 	}
-
-	flag.StringVar(&configPath, "config", defaultConfigPath, "path to configuration file")
-	flag.StringVar(&action, "action", "version", "action to run")
-	flag.StringVar(&inGlob, "in", "", "files to process")
-	flag.BoolVar(&force, "f", false, "force overwrite of existing workspace data")
-
-}
-
-func readConfiguration() {
-	file, err := os.Open(configPath)
+	_, err = os.Stat(working)
 	if err != nil {
-		log.Fatalf("Could not open %s: %s", configPath, err)
+		if os.IsNotExist(err) {
+			log.Fatalf("PHOSPHORUS_DIR does not exist: %s", working)
+		}
+		log.Fatalln(err)
 	}
+
+	if err = os.Chdir(working); err != nil { log.Fatalln(err) }
+
+	file, err := os.Open(CONFIG_FILE)
+	if err != nil { log.Fatalln(err) }
 	defer file.Close()
 
-	err = json.NewDecoder(file).Decode(&Config)
-	if err != nil {
-		log.Fatalf("Error parsing configuration file %s: %s", configPath, err)
-	}
+	if json.NewDecoder(file).Decode(&config) != nil { log.Fatalln(err) }
+
+	runtime.GOMAXPROCS(config.MaxProcs)
 }
 
-
 func main() {
-	flag.Parse()
-	readConfiguration()
+	fmt.Println(BANNER)
 
-	runtime.GOMAXPROCS(Config.MaxProcs)
-	log.Println(BANNER)
+	action := os.Args[1]
 	switch action {
-	// case "version":
-	// 	flag.PrintDefaults()
-	// case "prepare":
-	// 	Counts()
-	// case "genhash":
-	// 	GenerateHashFamily()
-	case "tables":
-		CreateTables()
-	case "testdynamo":
-		TestTables()
-	// case "index":
-	// 	Index()
+	case "sure":
+		log.Println("ok")
 	default:
 		log.Printf("Unrecognized action %q", action)
 	}
-	log.Println("Goodbye")
 }
