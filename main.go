ee47f8f3ab5310b84db2553e9bbc19d7fa93f14c
Will St. Clair <will@willstclair.com>
2014-02-07 13:45:17 -0500

    Drop omnibus configuration file from main

--- main.go
+++ main.go
@@ -1,21 +1,17 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 	"runtime"
 	"strings"
-	"willstclair.com/phosphorus/config"
 )
 
 const (
-	CONFIGFILE = "config.json"
-	BANNER     = `
+	BANNER = `
                            )
                           ) \
       . . . . . . . . . ./ ) (. .
@@ -48,13 +44,8 @@ var commands = []*Command{
 	cmdMem2,
 }
 
-var confPath string
-var confFrom string
 var noBanner bool
-var conf config.Configuration
 var maxprocs int
-
-const CONFIGENV = "PHOSPHORUS_CONFIG"
 
 func (c *Command) Name() string {
 	name := c.UsageLine
@@ -66,7 +57,7 @@ func (c *Command) Name() string {
 }
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "usage: phosphorus [ -c config.json ] command\n\n")
+	fmt.Fprintf(os.Stderr, "usage: phosphorus [args] command [args]\n\n")
 	os.Exit(2)
 }
 
@@ -78,27 +69,7 @@ func main() {
 
 	// log.SetFlags(0)
 
-	if err := findConfig(); err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n\n", err.Error())
-		os.Exit(1)
-	}
-
-	if err := loadConfig(); err != nil {
-		switch err.(type) {
-		default:
-			log.Printf("error loading configuration file: %s\n", err)
-		case *config.Error:
-			fmt.Printf("phosphorus: invalid configuration file:\n\n")
-			for m := range err.(*config.Error).Messages() {
-				fmt.Printf("\t%s\n", m)
-			}
-			fmt.Printf("\n")
-
-		}
-		os.Exit(1)
-	}
-
-	http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost = conf.MaxIdleConnsPerHost
+	http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost = 100
 
 	args := flag.Args()
 	if len(args) < 1 {
@@ -108,8 +79,6 @@ func main() {
 	if !noBanner {
 		fmt.Fprintf(os.Stderr, BANNER)
 	}
-
-	configInfo()
 
 	for _, cmd := range commands {
 		if cmd.Name() == args[0] {
@@ -129,52 +98,8 @@ func main() {
 }
 
 func init() {
-	flag.StringVar(&confPath, "c", "", "configuration file")
 	flag.BoolVar(&noBanner, "nobanner", false, "do not show banner")
 	flag.IntVar(&maxprocs, "p", 2, "go MAXPROCS")
-}
-
-func findConfig() (err error) {
-	if confPath != "" {
-		if confPath, err = filepath.Abs(confPath); err != nil {
-			return
-		}
-		confFrom = "commandline"
-		return
-	}
-
-	confPath = os.Getenv(CONFIGENV)
-	if confPath != "" {
-		if confPath, err = filepath.Abs(confPath); err != nil {
-			return
-		}
-
-		confFrom = "environment"
-		return
-	}
-
-	err = errors.New(fmt.Sprintf("phosphorus: specify a configuration file with $%s or -c",
-		CONFIGENV))
-	return
-}
-
-func loadConfig() (err error) {
-	file, err := os.Open(confPath)
-	if err != nil {
-		return
-	}
-	defer file.Close()
-
-	conf = config.Configuration{}
-	if err = conf.Load(file); err != nil {
-		return
-	}
-
-	return
-}
-
-func configInfo() {
-	log.Printf("Configuration path: %s (from %s)\n\n", confPath, confFrom)
 }
 
 func msg(resource, disposition string) {
